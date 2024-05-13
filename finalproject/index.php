fd26fd8197ab37e44021f75fbcf78b297e388903
lpaulino4543 <64877774+lpaulino4543@users.noreply.github.com>
2024-05-13 10:42:07 -0400

    Add files via upload

---
+++ finalproject/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include "bootstrap.head.php"; ?>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Company Investments</h1>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Report 1<a href="custom-report.php?companyId=A001"> goto </a></h3>
+      <p>Allianz Life Insurance Co Of North America</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 2<a href="custom-report.php?companyId=B002"> goto </a></h3>
+      <p>Berkshire Hathaway Inc</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 3<a href="custom-report.php?companyId=B003"> goto </a></h3>        
+      <p>Bill  Melinda Gates Foundation Trust</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 4<a href="custom-report.php?companyId=B004"> goto </a></h3>
+      <p>BlackRock Inc </p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 5<a href="custom-report.php?companyId=C005"> goto </a></h3>
+      <p>Citigroup Inc </p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 6<a href="custom-report.php?companyId=G006"> goto </a></h3>        
+      <p>Goldman Sachs Group</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 7<a href="custom-report.php?companyId=T007"> goto </a></h3>
+      <p>Td Ameritrade Investment Management LLC</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
